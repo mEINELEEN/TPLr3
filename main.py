a78aba5ac74590bde0ea458a012cdbd3bd403037
mEINELEEN <isalimov1978@gmail.com>
2025-06-01 20:49:30 +0300

    Добавлен макет окна

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QTabWidget,
 )
 
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Статистика по заданию")
+        self.resize(900, 600)
+
+        layout = QVBoxLayout()
+
+        self.tabs = QTabWidget()
+
+        # Сюда можно добавлять другие вкладки
+
+        layout.addWidget(self.tabs)
+        self.setLayout(layout)
+
