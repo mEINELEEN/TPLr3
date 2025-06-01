9a16b042ded20dc4bd73438b603b262b721b14e4
mEINELEEN <isalimov1978@gmail.com>
2025-06-01 20:54:22 +0300

    Добавлен модуль children с библиотеками

---
+++ children.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QPushButton,
+    QLabel, QTableWidget, QTableWidgetItem,
+    QSpinBox, QHBoxLayout
+)
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+
